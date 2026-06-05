783a55f33c779f93486587b282b0fbb952ee6de4
amazon129810-byte <amazon129810@gmail.com>
2026-06-05 13:37:30 -0700

    chore: Add root build.gradle.kts configuration

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    kotlin("android") version "1.9.21" apply false
+    kotlin("plugin.serialization") version "1.9.21" apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+}
+
+allprojects {
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
